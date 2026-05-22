2806cf2584031a1452263bdb3a7c910fbc64bf77
sallyom <somalley@redhat.com>
2026-05-22 16:56:01 -0400

    remove podman farm from build script
    Signed-off-by: sallyom <somalley@redhat.com>

--- openshell/build-openclaw-source-image.sh
+++ openshell/build-openclaw-source-image.sh
@@ -5,14 +5,14 @@ image="${1:-openclaw-openshell:local}"
 openclaw_dir="${OPENCLAW_DIR:-../openclaw}"
 base_image="${OPENCLAW_BASE_IMAGE:-${image}-base}"
 
-#podman farm build \
-#  -t "$base_image" \
-#  -f "$openclaw_dir/Dockerfile" \
-#  --build-arg OPENCLAW_EXTENSIONS=diagnostics-otel,codex \
-#  --build-arg OPENCLAW_IMAGE_APT_PACKAGES="openssh-client rsync" \
-#  "$openclaw_dir"
+podman build \
+  -t "$base_image" \
+  -f "$openclaw_dir/Dockerfile" \
+  --build-arg OPENCLAW_EXTENSIONS=diagnostics-otel,codex \
+  --build-arg OPENCLAW_IMAGE_APT_PACKAGES="openssh-client rsync" \
+  "$openclaw_dir"
 
-podman farm build \
+podman build \
   -t "$image" \
   -f openshell/Dockerfile \
   --build-arg OPENCLAW_BASE_IMAGE="$base_image" \
